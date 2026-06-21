e5f92640dc3bf608d339d4cc2ad5579d81162021
Rohan Mia <rohanmia.org@gmail.com>
2026-06-22 02:10:32 +0600

    userCollection, lawyerProfilesCollection, all users get api and lawyerProfiles post api created

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const app = express()
-const cors = require("cors")
-require("dotenv").config()
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
@@ -28,6 +28,44 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const database = client.db("advokate");
+
+      //! user collection
+      const userCollection = database.collection("user");
+      
+    //! lawyerProfiles collection
+      const lawyerProfilesCollection = database.collection("lawyerProfiles");
+      
+
+
+
+
+
+      //=============================================================================user related api's=========================================================================
+      
+      //? get all the users
+      app.get("/api/user", async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+          });
+
+
+      
+      
+      //=============================================================================lawyer profile related api's==============================================================
+      app.post("/api/lawyerProfiles", async (req, res) => {
+          const legalProfile = req.body;
+          const newLegalProfile = {
+              ...legalProfile,
+              createdAt: new Date()
+          }
+          const result = await lawyerProfilesCollection.insertOne(newLegalProfile)
+          res.send(result);
+      })
+      
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
