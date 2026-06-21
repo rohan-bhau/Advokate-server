57207b70fe878f49e9e9bd10c30375ff25e71a99
Rohan Mia <rohanmia.org@gmail.com>
2026-06-22 01:43:18 +0600

    mongodb connected to the advokate-server

--- index.js
+++ index.js
@@ -6,10 +6,38 @@ require("dotenv").config()
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// ? mongodb uri
+const uri = process.env.MONGO_URI;
+
+//? mongo db connection
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
